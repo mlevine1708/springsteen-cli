6371294e7507208c3ec659a92f3a212e518494a6
Micah Levine <micah.levine@outlook.com>
2019-10-18 17:03:11 +0000

    first commit

---
+++ lib/springtseen/cli.rb
@@ -0,0 +1,8 @@
+require "springtseen/cli/version"
+
+module Springtseen
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/springtseen/cli/version.rb
@@ -0,0 +1,5 @@
+module Springtseen
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
