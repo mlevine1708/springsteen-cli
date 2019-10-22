d638840c2b9cf5131583f628c5f49247967612d8
Micah Levine <micah.levine@outlook.com>
2019-10-22 02:08:02 +0000

    start of classes

---
+++ lib/springsteen/album.rb
@@ -0,0 +1,16 @@
+class Album
+  
+  attr_accessor :title, :date, :url 
+  
+  
+  def self.list 
+    
+    album_1 = self.new 
+    album_1.title = "Greetings From Asbury Park, NJ"
+    album_1.date = January 5, 1973
+    album_1.url = https://brucespringsteen.net/albums/greetings-from-asbury-park-n-j-2
+    
+  
+  
+  
+end
