cbe937f0351b8d1f519b62493fb5985c1f87a393
Micah Levine <micah.levine@outlook.com>
2019-10-31 10:07:33 +0000

    object add

--- lib/springsteen/album.rb
+++ lib/springsteen/album.rb
@@ -6,35 +6,21 @@ def initialize
   @title = title
   @date = date
   @url = url 
- 
- #def self.scrape_springsteen
-  # doc = Nokogiri::HTML(open("https://brucespringsteen.net/albums"))
-   
-   #album_1 = self.new
-   #album_1.title = doc.search("h2.title").text 
-   
-   site = "https://brucespringsteen.net/albums"
-doc = Nokogiri::HTML(open(site))
-
-albums = doc.css("div.album-minis a.album-mini")
-puts albums.count
-
-albums.each do |album|
-  url = album.attr("href")
-  title = album.css("span.title").text
-  date = album.css("span.date").text
-  puts title, date, url, ""
+  @@all << self 
 end
+
+def self.all 
+  @@self 
+end
+
+ 
+ 
+   
+   
    
  end
   
-  #def self.list 
-    
-   # album_1 = self.new 
-    #album_1.title = "Greetings From Asbury Park, NJ"
-    #album_1.date = January 5, 1973
-    #album_1.url = https://brucespringsteen.net/albums/greetings-from-asbury-park-n-j-2
-    #album_1.description = 
+  
     
   
   
