d1b088d12030f7affb0f516c95359f9aefda4eaf
Micah Levine <micah.levine@outlook.com>
2019-11-11 02:55:15 +0000

    new attempt

--- lib/springsteen/cli.rb
+++ lib/springsteen/cli.rb
@@ -2,21 +2,25 @@ module Springsteen
 class CLI 
   
  def call
-   puts "Welcome to the Bruce Springsteen CLI"
-   @input = ""
-   until @input == "exit"
+   puts "Welcome to the Bruce Springsteen CLI! 
+Would you like to see a list of Bruce Springsteen's albums?"
+   input = ""
+   input = gets.strip 
+   until input == "exit"
+   if input = yes then 
   self.load_albums
   find_descriptions
   sign_off
 end
+end 
 
  def self.load_albums
-   @album = Springsteen::Album.all
+   album = Springsteen::Album.all
  end
  
  def find_descriptions
    puts "If you want to learn more about an album, enter its number: "
-    @album.each.with_index(1) do |album, index| 
+    album.each.with_index(1) do |album, index| 
       puts "#{index}. #{album.name}"
    end
  end
@@ -41,5 +45,7 @@ end
       puts "#{i}. #{album.title} - #{album.date} - #{album.url}"
     end
   end
+end 
+
   
-end
+
